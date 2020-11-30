3dba251b4347f55a079d4f55cc4225cb7782986c
Osama Elkorashy <https://github.com/OsamaElkorashy>
2020-11-30 14:51:08 +0200

    Add project files.

---
+++ Plugins/Nop.Plugin.Misc.DiscountSearch/DiscountSearch.cs
@@ -0,0 +1,49 @@
+﻿using Nop.Services.Cms;
+using Nop.Services.Plugins;
+using System;
+using System.Collections.Generic;
+
+namespace NOP.Plugin.Misc.Search
+{
+    public class DiscountSearch : IWidgetPlugin
+    {
+        public bool HideInWidgetList => throw new NotImplementedException();
+
+        public PluginDescriptor PluginDescriptor { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public string GetConfigurationPageUrl()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetWidgetViewComponentName(string widgetZone)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<string> GetWidgetZones()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Install()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PreparePluginToUninstall()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Uninstall()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(string currentVersion, string targetVersion)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Presentation/Nop.Web/Models/Catalog/DisountProductsModel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Nop.Web.Framework.Mvc.ModelBinding;
+using Nop.Web.Framework.Models;
+
+namespace Nop.Web.Models.Catalog
+{
+    public partial class DisountProductsModel : BaseNopModel
+    {
+        public DisountProductsModel()
+        {
+            PagingFilteringContext = new CatalogPagingFilteringModel();
+            Products = new List<ProductOverviewModel>();
+        }
+
+        /// <summary>
+        /// Discount ID
+        /// </summary>
+        [NopResourceDisplayName("Search.Discount")]
+        public int did { get; set; }
+        public CatalogPagingFilteringModel PagingFilteringContext { get; set; }
+        public IList<ProductOverviewModel> Products { get; set; }
+
+    }
+}
